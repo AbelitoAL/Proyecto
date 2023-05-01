7000b10f8c199127b31f3e13c126514624d0c7a9
fabio <fabioarredondo44@gmail.com>
2023-04-30 21:56:15 -0400

    cambios

--- src/app.js
+++ src/app.js
@@ -1,4 +1,13 @@
 import express from "express"
+import fetch from "node-fetch";
+
+
+fetch("https://reqres.in/api/users?page=2")
+    .then((respueta)=> {
+        return respueta.json()
+    }).then((resp) => {
+        console.log(resp)
+    })
 
 const app = express();
 
