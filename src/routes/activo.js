586b79ac172e5011d0847ad77e44eb601a90e006
fabio <fabioarredondo44@gmail.com>
2023-05-16 23:21:30 -0400

    first commit

--- src/routes/activo.js
+++ src/routes/activo.js
@@ -7,7 +7,7 @@ const Acti = Router();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 export const storage = multer.diskStorage({
     destination: (req,file,cb) => {
-        cb(null, join(__dirname,'../public/images'))
+        cb(null, join(__dirname,'../public/img'))
     },
     filename: (req,file,cb) =>{
         const ext = file.originalname.split('.').pop()
